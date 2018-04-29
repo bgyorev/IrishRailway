fc0504224847043b0460d6e57e9b15fa88579f88
bgyorev <bozhidar.x.gyorev@gsk.com>
2018-04-29 14:58:27 +0300

    Added basic code for the stops view controller

--- IrishRail/Controller/StopsViewController.swift
+++ IrishRail/Controller/StopsViewController.swift
@@ -8,28 +8,27 @@
 
 import UIKit
 
-class StopsViewController: UIViewController {
+class StopsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var stopsTableView: UITableView!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        stopsTableView.delegate = self
+		stopsTableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
+	
 }
