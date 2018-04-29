9bc2a653ea0c3c548371d3b306a9b3f10bbbb2b9
bgyorev <bozhidar.x.gyorev@gsk.com>
2018-04-29 10:10:13 +0300

    Skeleton code for main and schedule view controllers

--- IrishRail/Controller/MainViewController.swift
+++ IrishRail/Controller/MainViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+	@IBOutlet weak var checkTrainButton: UIButton!
+	@IBOutlet weak var stationTextField: UITextField!
+	
+	private let picker = UIPickerView()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		picker.delegate = self
+		picker.dataSource = self
+		stationTextField.inputView = picker
+		
+		let toolbar = UIToolbar()
+		toolbar.barStyle = .default
+		toolbar.isTranslucent = true
+		
+		
+		
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	
+	// MARK: UIPickerViewDataSource
+	func numberOfComponents(in pickerView: UIPickerView) -> Int {
+		return 1
 	}
-
-
+	
+	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+		return 0
+	}
+	
+	// MARK: UIPickerViewDelegate
+	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+		return ""
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+		
+	}
+	
+	// MARK: IBActions
+	@IBAction func checkTrainButtonPressed(_ sender: Any) {
+	}
+	
 }
 
--- IrishRail/Controller/ScheduleViewController.swift
+++ IrishRail/Controller/ScheduleViewController.swift
@@ -8,28 +8,29 @@
 
 import UIKit
 
-class ScheduleViewController: UIViewController {
+class ScheduleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var scheduleTableView: UITableView!
+	override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	// MARK: UITableViewDataSource
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+	
+	// MARK: UITableViewDelegate
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return UITableViewCell()
+	}
 
 }
