9d0f9c034918efb4125e477923ba9985fbb1ba54
bgyorev <bozhidar.x.gyorev@gsk.com>
2018-04-29 12:41:24 +0300

    Web service methdos incorporated into the view controllers

--- IrishRail/Controller/MainViewController.swift
+++ IrishRail/Controller/MainViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+	// IBOutlets and views
 	@IBOutlet weak var checkTrainButton: UIButton!
 	@IBOutlet weak var stationTextField: UITextField!
 	
 	private let picker = UIPickerView()
+	
+	// Controller properties
+	private let webserviceHelper = WebserviceHelper.sharedInstance
+	private var stations = [Dictionary<String, String?>]()
+	private var selectedStation: Dictionary<String, String?>?
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -37,6 +43,26 @@ class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
 		stationTextField.inputAccessoryView = toolbar
 		
 	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		webserviceHelper.getAllStations { (stations) in
+			guard stations.count != 0 else {
+				self.showAlert(withTitle: "Stations error", message: "Application couldn't fetch railway stations list")
+				return
+			}
+
+			self.stations = stations.sorted(by: { (station1, station2) -> Bool in
+				if let s1 = station1[Constants.xmlFields.stationDescription], let s2 = station2[Constants.xmlFields.stationDescription] {
+					if let s1 = s1, let s2 = s2 {
+						return s1 < s2
+					}
+				}
+				return false
+			})
+			picker.reloadComponent(0)
+		}
+		
+	}
 
 	// MARK: Private methods
 	@objc
@@ -47,6 +73,7 @@ class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
 	@objc
 	private func cancelButtonPressed() {
 		self.doneButtonPressed()
+		selectedStation = nil
 		stationTextField.text = ""
 	}
 	
@@ -56,20 +83,46 @@ class MainViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDa
 	}
 	
 	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-		return 0
+		return stations.count
 	}
 	
 	// MARK: UIPickerViewDelegate
 	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+		if stations.count != 0 {
+			let row = stations[row]
+			if let stationDescription = row[Constants.xmlFields.stationDescription], let stationCode = row[Constants.xmlFields.stationCode] {
+				if let stationDescription = stationDescription, let stationCode = stationCode {
+					return "\(stationDescription)(\(stationCode))"
+				}
+				
+			}
+		}
 		return ""
 	}
 	
 	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-		
+		if stations.count != 0 {
+			let row = stations[row]
+			if let stationDescription = row[Constants.xmlFields.stationDescription], let stationCode = row[Constants.xmlFields.stationCode] {
+				if let stationDescription = stationDescription, let stationCode = stationCode {
+					self.stationTextField.text = "\(stationDescription)(\(stationCode))"
+					self.selectedStation = row
+				}
+				
+			}
+		}
 	}
 	
 	// MARK: IBActions
 	@IBAction func checkTrainButtonPressed(_ sender: Any) {
+		if let selectedStation = self.selectedStation, let stationCodeOptional = selectedStation[Constants.xmlFields.stationCode], let stationCode = stationCodeOptional {
+			webserviceHelper.getStationTrains(byCode: stationCode) { (trains) in
+				let scheduleVC = self.storyboard?.instantiateViewController(withIdentifier: "ScheduleVC") as! ScheduleViewController
+				scheduleVC.trains = trains
+	
+				self.navigationController?.pushViewController(scheduleVC, animated: true)
+			}
+		}
 	}
 	
 }
--- IrishRail/Controller/ScheduleViewController.swift
+++ IrishRail/Controller/ScheduleViewController.swift
@@ -11,10 +11,21 @@ import UIKit
 class ScheduleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
 	@IBOutlet weak var scheduleTableView: UITableView!
+	open var trains: [Dictionary<String, String?>]?
+	
 	override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+		scheduleTableView.delegate = self
+		scheduleTableView.dataSource = self
+		if let trains = trains {
+			if let first = trains.first {
+				if let stationCode = first[Constants.xmlFields.stationCode.lowercased().capitalized] {
+					self.navigationItem.title = stationCode!
+				}
+			}
+		}
     }
 
 	// MARK: UITableViewDataSource
@@ -24,12 +35,21 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
 	}
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if let trains = trains {
+			return trains.count
+		}
 		return 0
 	}
 	
 	// MARK: UITableViewDelegate
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if let trains = trains {
+			let train = trains[indexPath.row]
+			let cell = tableView.dequeueReusableCell(withIdentifier: "scheduleCell") as! ScheduleTableViewCell
+			cell.setupCell(withDictionary: train)
+			return cell
+		}
 		return UITableViewCell()
 	}
 
