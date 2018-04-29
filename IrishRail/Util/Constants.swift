4f22e1a161ef195a06992375e887552698b3875f
bgyorev <bozhidar.x.gyorev@gsk.com>
2018-04-29 10:22:22 +0300

    Added files for constants and webservice helper

---
+++ IrishRail/Util/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  IrishRail
+//
+//  Created by Bozhidar Gyorev on 29.04.18.
+//  Copyright © 2018 Bozhidar Gyorev. All rights reserved.
+//
+
+import Foundation
---
+++ IrishRail/Util/WebserviceHelper.swift
@@ -0,0 +1,17 @@
+//
+//  WebserviceHelper.swift
+//  IrishRail
+//
+//  Created by Bozhidar Gyorev on 29.04.18.
+//  Copyright © 2018 Bozhidar Gyorev. All rights reserved.
+//
+
+import Foundation
+
+class WebserviceHelper: NSObject {
+	open static let sharedInstance = WebserviceHelper()
+	
+	open func getAllStations() {
+		
+	}
+}
